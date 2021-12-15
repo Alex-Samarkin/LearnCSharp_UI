0bee9aae4b23871d88367e033f80748d0ed06407
alex_ <alexsamarkinru@gmail.com>
2021-12-15 15:28:43 +0300

    Добавьте файлы проекта.

---
+++ SmallBrowser_01/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SmallBrowser_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.окноОПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.псковГУToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.инструкцииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripSplitButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 462);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(904, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(85, 17);
+            this.toolStripStatusLabel1.Text = "О программе:";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.инструкцииToolStripMenuItem,
+            this.псковГУToolStripMenuItem,
+            this.окноОПрограммеToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 20);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            // 
+            // окноОПрограммеToolStripMenuItem
+            // 
+            this.окноОПрограммеToolStripMenuItem.Name = "окноОПрограммеToolStripMenuItem";
+            this.окноОПрограммеToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.окноОПрограммеToolStripMenuItem.Text = "Окно \"О программе\"";
+            // 
+            // псковГУToolStripMenuItem
+            // 
+            this.псковГУToolStripMenuItem.Name = "псковГУToolStripMenuItem";
+            this.псковГУToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.псковГУToolStripMenuItem.Text = "2021 - ПсковГУ";
+            // 
+            // инструкцииToolStripMenuItem
+            // 
+            this.инструкцииToolStripMenuItem.Name = "инструкцииToolStripMenuItem";
+            this.инструкцииToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.инструкцииToolStripMenuItem.Text = "Инструкции";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 484);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Веб-браузер";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem псковГУToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem окноОПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem инструкцииToolStripMenuItem;
+    }
+}
+
